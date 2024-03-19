d96ee33d2dd0e11b75bf3fa40ca17711843760a5
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-18 19:43:36 -0500

    Its working for now

--- assets/script/app.js
+++ assets/script/app.js
@@ -13,8 +13,13 @@ const form = select('form');
 const inputHour = select('.hour');
 const inputMinutes = select('.minutes');
 const setAlarm = select('.set');
-const message = select('.message');
+const message = select('.message p');
 const output = select('.alarm-set-text');
+
+const sound = new Audio('./assets/audio/alarm.mp3');
+sound.type = 'audio/mp3';
+
+const alarm = new Date();
 
 function isValid(input) {
     if (input.length > 0 && input.length <= 2 && !isNaN(input) && input < 60 ) return true;
@@ -26,11 +31,7 @@ function clearInputs() {
     inputMinutes.value = '';
 }
 
-function getTime() {
-    const now = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-
+function addZeros(hours, minutes) {
     if (hours < 10) {
         hours = `0${hours}`;
     }
@@ -39,7 +40,17 @@ function getTime() {
         minutes = `0${minutes}`;
     }
 
-    currentTimeOutput.innerText = `${hours}:${minutes}`;
+    return { hours, minutes };
+}
+
+function getTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    const paddedTime = addZeros(hours, minutes);
+
+    currentTimeOutput.innerText = `${paddedTime.hours}:${paddedTime.minutes}`;
 
     setTimeout(() => {
         getTime();
@@ -50,26 +61,45 @@ getTime();
 
 
 function getAlarm() {
-    let alarm = new Date();
-
     let hours = inputHour.value;
     let minutes = inputMinutes.value;
 
-    if (hours < 10) {
-        hours = `0${hours}`;
+    if (isValid(hours) && isValid(minutes)) {
+        addZeros(hours, minutes);
+        const paddedTime = addZeros(hours, minutes);
+        output.innerText = `${paddedTime.hours}:${paddedTime.minutes}`;
+        clearInputs();
+    } else {
+        message.innerText = 'Please enter Valid times';
     }
 
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-
-    output.innerText = `${hours}:${minutes}`;
+    alarm.setHours(hours);
+    alarm.setMinutes(minutes);
 }
 
-// listen('setAlarm', click, () => {
-//     getAlarm();
-// });
+function observe() {
+    const now = new Date();
+    if ((alarm.getHours() === now.getHours()) && (alarm.getMinutes() === now.getMinutes())) {
+        sound.play();
+    }
 
-setAlarm.addEventListener('click', function() {
+    setTimeout(() => {
+        observe();
+    }, 60000);
+}
+
+listen('click', setAlarm, () => {
     getAlarm();
 });
+
+
+listen('load', window, () => {
+    let h = inputHour.value;
+    let m = inputMinutes.value;
+
+    alarm.setHours(h);
+    alarm.setMinutes(m);
+    observe();
+});
+
+window.addEventListener('load', observe());
